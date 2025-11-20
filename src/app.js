f17d145668048dbf3469393ec0e16be642610f12
Alex649814 <L21330388@hermosillo.tecnm.mx>
2025-11-20 15:43:59 -0700

    Instalar cors

--- src/app.js
+++ src/app.js
@@ -1,22 +1,14 @@
-
-import express from "express";
-import cors from "cors";
-
-import catalogoRoutes from "./routes/catalogo.routes.js";
-import disponibilidadRoutes from "./routes/disponibilidad.routes.js";
-import ventasMallRoutes from "./routes/ventasMall.routes.js";
-import pagosRoutes from "./routes/pagos.routes.js";
-import bancoRoutes from "./routes/banco.routes.js";
-import citasWebRoutes from "./routes/citasWeb.routes.js";
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-app.use("/api", catalogoRoutes);
-app.use("/api", disponibilidadRoutes);
-app.use("/api", ventasMallRoutes);
-app.use("/api", pagosRoutes);
-app.use("/api", bancoRoutes);
-app.use("/api/citas", citasWebRoutes);
-
+import express from "express"; 
+import catalogoRoutes from "./routes/catalogo.routes.js"; 
+import disponibilidadRoutes from "./routes/disponibilidad.routes.js"; 
+import ventasMallRoutes from "./routes/ventasMall.routes.js"; 
+import pagosRoutes from "./routes/pagos.routes.js"; 
+import bancoRoutes from "./routes/banco.routes.js"; 
+const app = express(); 
+app.use(express.json()); 
+app.use("/api", catalogoRoutes); 
+app.use("/api", disponibilidadRoutes); 
+app.use("/api", ventasMallRoutes); 
+app.use("/api", pagosRoutes); 
+app.use("/api", bancoRoutes); 
 export default app;
